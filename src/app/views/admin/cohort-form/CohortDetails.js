3ebcc3f8d027b94752c5f8f55705842b7734258d
Omonroy36 <monroyorlando.36@gmail.com>
2020-11-13 03:16:11 +0000

    cohort details updated

--- src/app/views/admin/cohort-form/CohortDetails.js
+++ src/app/views/admin/cohort-form/CohortDetails.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Divider, Card, Grid, TextField, Button, MenuItem } from "@material-ui/core";
 import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
 } from "@material-ui/pickers";
 import { Formik } from "formik";
 import { Link } from "react-router-dom";
@@ -10,133 +10,137 @@ import { Rating } from "@material-ui/lab";
 import DateFnsUtils from "@date-io/date-fns";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
+import axios from "../../../../axios";
 
 const useStyles = makeStyles(({ palette, ...theme }) => ({
-  avatar: {
-    border: "4px solid rgba(var(--body), 0.03)",
-    boxShadow: theme.shadows[3],
-  },
+    avatar: {
+        border: "4px solid rgba(var(--body), 0.03)",
+        boxShadow: theme.shadows[3],
+    },
 }));
 
-const InvoiceCustomer = () => {
-  const classes = useStyles();
+const CohortDetails = ({ slug, endDate, startDate, lang, id }) => {
+    console.log(id)
+    const classes = useStyles();
+    const onSubmit = (values) => {
+        axios.put(`${process.env.REACT_APP_API_HOST}/v1/admissions/cohort/${id}`)
+    }
+    return (
+        <Card className="p-4">
+            <div className="mb-4 flex justify-between items-center">
+                <h4 className="m-0 font-medium">Cohort Details</h4>
+            </div>
 
-  return (
-    <Card className="p-4">
-      <div className="mb-4 flex justify-between items-center">
-        <h4 className="m-0 font-medium">Cohort Details</h4>
-      </div>
+            <Divider className="mb-6" />
 
-      <Divider className="mb-6" />
+            <Formik
+                initialValues={{
+                    slug: slug,
+                    lang: lang,
+                    ending_date: endDate,
+                    kickoff_date: startDate
+                }}
+                onSubmit={(values) => console.log(values)}
+                enableReinitialize={true}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                    setSubmitting,
+                    setFieldValue,
+                }) => (
+                        <form className="p-4" onSubmit={handleSubmit}>
+                            <Grid container spacing={3} alignItems="center">
+                                <Grid item md={4} sm={4} xs={12}>
+                                    Cohort Slug
+                                </Grid>
+                                <Grid item md={8} sm={8} xs={12}>
+                                    <TextField
+                                        label="Slug"
+                                        name="slug"
+                                        size="small"
+                                        variant="outlined"
+                                        value={values.slug}
+                                        onChange={handleChange}
+                                    />
+                                </Grid>
+                                <Grid item md={3} sm={4} xs={12}>
+                                    Start date
+                </Grid>
+                                <Grid item md={9} sm={8} xs={12}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardDatePicker
+                                            className="m-2"
+                                            margin="none"
+                                            label="Date"
+                                            inputVariant="outlined"
+                                            type="text"
+                                            size="small"
+                                            autoOk={true}
+                                            value={values.kickoff_date}
+                                            format="MMMM dd, yyyy"
+                                            onChange={(date) => setFieldValue("kickoff_date", date)}
+                                        />
+                                    </MuiPickersUtilsProvider>
 
-        <Formik
-          initialValues={initialValues}
-        //   onSubmit={handleSubmit}
-          enableReinitialize={true}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            setSubmitting,
-            setFieldValue,
-          }) => (
-            <form className="p-4" onSubmit={handleSubmit}>
-              <Grid container spacing={3} alignItems="center">
-                <Grid item md={4} sm={4} xs={12}>
-                  Cohort Slug
+                                </Grid>
+                                <Grid item md={3} sm={4} xs={12}>
+                                    End date
                 </Grid>
-                <Grid item md={8} sm={8} xs={12}>
-                  <TextField
-                    label="Slug"
-                    name="cohortSlug"
-                    size="small"
-                    variant="outlined"
-                    defaultValue="santiago-xv"
-                    value={""}
-                    onChange={handleChange}
-                  />
+                                <Grid item md={9} sm={8} xs={12}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardDatePicker
+                                            className="m-2"
+                                            margin="none"
+                                            label="Date"
+                                            inputVariant="outlined"
+                                            type="text"
+                                            size="small"
+                                            autoOk={true}
+                                            value={values.ending_date}
+                                            format="MMMM dd, yyyy"
+                                            onChange={(date) => setFieldValue("ending_date", date)}
+                                        />
+                                    </MuiPickersUtilsProvider>
+
+                                </Grid>
+                                <Grid item md={3} sm={4} xs={12}>
+                                    Language
                 </Grid>
-                <Grid item md={3} sm={4} xs={12}>
-                  Start date
-                </Grid>
-                <Grid item md={9} sm={8} xs={12}>
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                      <KeyboardDatePicker
-                        className="m-2"
-                        margin="none"
-                        label="Date"
-                        inputVariant="outlined"
-                        type="text"
-                        size="small"
-                        autoOk={true}
-                        value={values.invoiceDate}
-                        format="MMMM dd, yyyy"
-                        onChange={(date) => setFieldValue("invoiceDate", date)}
-                      />
-                    </MuiPickersUtilsProvider>
-                    
-                </Grid>
-                <Grid item md={3} sm={4} xs={12}>
-                  End date
-                </Grid>
-                <Grid item md={9} sm={8} xs={12}>
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                      <KeyboardDatePicker
-                        className="m-2"
-                        margin="none"
-                        label="Date"
-                        inputVariant="outlined"
-                        type="text"
-                        size="small"
-                        autoOk={true}
-                        value={values.invoiceDate}
-                        format="MMMM dd, yyyy"
-                        onChange={(date) => setFieldValue("invoiceDate", date)}
-                      />
-                    </MuiPickersUtilsProvider>
-                    
-                </Grid>
-                <Grid item md={3} sm={4} xs={12}>
-                  Language
-                </Grid>
-                <Grid item md={9} sm={8} xs={12}>
-                    <TextField
-                      className="m-2 min-w-188"
-                      label="Language"
-                      name="terms"
-                      size="small"
-                      variant="outlined"
-                      value={values.terms || ""}
-                      onChange={handleChange}
-                      select
-                    >
-                      {['es', 'en'].map((item, ind) => (
-                        <MenuItem value={item} key={item}>
-                          {item}
-                        </MenuItem>
-                      ))}
-                    </TextField>
-                    
-                </Grid>
-                <Button  color="primary" variant="contained" type="submit">
-                  Save Cohort Details
+                                <Grid item md={9} sm={8} xs={12}>
+                                    <TextField
+                                        className="m-2 min-w-188"
+                                        label="Language"
+                                        name="lang"
+                                        size="small"
+                                        variant="outlined"
+                                        value={values.lang}
+                                        onChange={handleChange}
+                                        select
+                                    >
+                                        {['es', 'en'].map((item, ind) => (
+                                            <MenuItem value={item} key={item}>
+                                                {item}
+                                            </MenuItem>
+                                        ))}
+                                    </TextField>
+
+                                </Grid>
+                                <Button color="primary" variant="contained" type="submit">
+                                    Save Cohort Details
                 </Button>
-            </Grid>
-          </form>
-        )}
-        </Formik>
-    </Card>
-  );
+                            </Grid>
+                        </form>
+                    )}
+            </Formik>
+        </Card>
+    );
 };
 
-const initialValues = {
-  slug: "",
-  name: "Miami Dade XV",
-};
 
-export default InvoiceCustomer;
+export default CohortDetails;
--- src/app/views/admin/cohort-form/index.js
+++ src/app/views/admin/cohort-form/index.js
@@ -1,44 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IconButton, Icon, Button, Grid } from "@material-ui/core";
 import { Link, useParams } from "react-router-dom";
 import CohortStudents from "./CohortStudents";
 import CohortDetails from "./CohortDetails";
+import { MatxLoading } from "matx";
+import axios from "../../../../axios";
 
-const Invoice2 = () => {
+const Cohort = () => {
     const { slug } = useParams();
+    const [isLoading, setIsLoading] = useState(false);
+    const [snackOpen, setSnackOpen] = useState(false);
+    const [cohort, setCohort] = useState({})
+    useEffect(() => {
+        getCohort();
+    }, [])
 
-  return (
-    <div className="m-sm-30">
-      <div className="flex flex-wrap justify-between mb-6">
-        <div>
-          <h3 className="mt-0 mb-4 font-medium text-28">Cohort: {slug}</h3>
-          <div className="flex">
-            <div className="px-3 text-11 py-3px border-radius-4 text-white bg-green mr-3">
-              FINAL_PROJECT
+    const handleClose = () => setSnackOpen(true);
+
+    const getCohort = () => {
+        setIsLoading(true);
+        axios.get(`${process.env.REACT_APP_API_HOST}/v1/admissions/cohort/${slug}`)
+            .then(({ data }) => {
+                setIsLoading(false);
+                setCohort(data);
+            })
+            .catch(error => console.log(error));
+    }
+
+    return (
+        <div className="m-sm-30">
+            <div className="flex flex-wrap justify-between mb-6">
+                <div>
+                    <h3 className="mt-0 mb-4 font-medium text-28">Cohort: {slug}</h3>
+                    <div className="flex">
+                        <div className="px-3 text-11 py-3px border-radius-4 text-white bg-green mr-3">
+                            {cohort && cohort.stage}
             </div>
-          </div>
+                    </div>
+                </div>
+                {isLoading && <MatxLoading />}
+            </div>
+            <Grid container spacing={3}>
+                <Grid item md={4} xs={12}>
+                    <CohortDetails 
+                    slug={slug} 
+                    lang={cohort.lang || "en"} 
+                    endDate={cohort.ending_date} 
+                    startDate={cohort.kickoff_date}
+                    id={cohort.id}
+                    />
+                </Grid>
+                <Grid item md={8} xs={12}>
+                    <CohortStudents slug={slug} />
+                </Grid>
+            </Grid>
         </div>
-
-        <div className="">
-          <IconButton className="mr-2">
-            <Icon>more_horiz</Icon>
-          </IconButton>
-          <Button variant="contained" color="primary">
-            Fulfill Order
-          </Button>
-        </div>
-      </div>
-
-      <Grid container spacing={3}>
-        <Grid item md={4} xs={12}>
-          <CohortDetails slug={slug} />
-        </Grid>
-        <Grid item md={8} xs={12}>
-          <CohortStudents slug={slug} />
-        </Grid>
-      </Grid>
-    </div>
-  );
+    );
 };
 
-export default Invoice2;
+export default Cohort;
