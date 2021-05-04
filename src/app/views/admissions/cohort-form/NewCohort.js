0989d1842bae741038dbf1e68189039a5890e3b2
jagtrrz <jagutierrezc7@gmail.com>
2021-05-04 20:50:47 +0200

    end dates deafult in null

--- src/app/views/admissions/cohort-form/NewCohort.js
+++ src/app/views/admissions/cohort-form/NewCohort.js
@@ -29,7 +29,6 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
 const NewCohort = () => {
   const classes = useStyles();
   const startDate = new Date();
-  const [endDates, setEndDates] = useState(new Date());
   const [cert, setCert] = useState(null);
   const [version, setVersion] = useState(null);
   const [checked, setChecked] = useState(false)
@@ -38,7 +37,7 @@ const NewCohort = () => {
     name: "",
     slug: "",
     kickoff_date: startDate,
-    ending_date: endDates,
+    ending_date: null,
     never_ends: false,
   })
   const academy = JSON.parse(localStorage.getItem("bc-academy"));
@@ -53,12 +52,6 @@ const NewCohort = () => {
           never_ends: true
     })
   };
-
-  useEffect(() => {
-    setEndDates(
-      endDates.setDate(endDates.getDate() + 30)
-    )
-  }, [])
   
   const createCohort = (event) => {
     setNewCohort({
@@ -153,6 +146,7 @@ const NewCohort = () => {
                     getOptionLabel={option => `${option.name}`}
                     value={cert} />
                       {cert !== null ? <AsyncAutocomplete
+                      debounced={false}
                       onChange={(v) => setVersion(v)}
                       width={"20%"}
                       key={cert.slug}
@@ -198,7 +192,6 @@ const NewCohort = () => {
                           inputVariant="outlined"
                           type="text"
                           size="small"
-                          autoOk={true}
                           value={newCohort.ending_date}
                           format="MMMM dd, yyyy"
                           onChange={(date) => setNewCohort({
@@ -207,6 +200,7 @@ const NewCohort = () => {
                                 never_ends: false,
                           })}
                           disabled={neverEnd ? false : true}
+                          required
                         />
                     </MuiPickersUtilsProvider>
                 </Grid>
