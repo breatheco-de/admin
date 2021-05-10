db1cf724cc452688042f0020ce83a06c645f87e5
Alexson <alexsoncoelho21@hotmail.com>
2021-05-10 17:01:00 -0400

    setting up delete bulk students

--- src/app/components/CustomToolbar.js
+++ src/app/components/CustomToolbar.js
@@ -1,91 +1,129 @@
-import React, { useState, useEffect } from "react";
-import DeleteIcon from '@material-ui/icons/Delete';
-import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import React, { useState, useEffect, useMemo } from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import GroupAddIcon from "@material-ui/icons/GroupAdd";
 import { withStyles } from "@material-ui/core/styles";
-import {AsyncAutocomplete} from "./Autocomplete";
+import { AsyncAutocomplete } from "./Autocomplete";
 import bc from "../services/breathecode";
 import {
-    DialogTitle,
-    Dialog,
-    Button,
-    Tooltip,
-    DialogActions,
-    IconButton,
+  DialogTitle,
+  Dialog,
+  Button,
+  Tooltip,
+  DialogActions,
+  IconButton,
 } from "@material-ui/core";
 
 const defaultToolbarSelectStyles = {
-    iconButton: {
-    },
-    iconContainer: {
-        marginRight: "24px",
-    },
-    inverseIcon: {
-        transform: "rotate(90deg)",
-    },
+  iconButton: {},
+  iconContainer: {
+    marginRight: "24px",
+  },
+  inverseIcon: {
+    transform: "rotate(90deg)",
+  },
 };
 
 const CustomToolbarSelect = (props) => {
-    const { classes } = props;
-    const [openDialog, setOpenDialog] = useState(false);
-    const [cohort, setCohort] = useState(null);
-    const [bulk, setBulk] = useState([])
-    useEffect(() => {
-        let selected = props.selectedRows.data.map(item => item.index);
-        setBulk(selected.map(item => { if(props.items[item].user !== null) return { user: props.items[item].user.id, role:"STUDENT", finantial_status: null ,educational_status: null }}))
-    }, [])
-    const addBulkToCohort = (e) => {
-        e.preventDefault();
-        bc.admissions().addUserCohort(cohort.id, bulk).then(d => d).catch(r => r);
-        setOpenDialog(false);
-    };
-    return (
-        <div className={classes.iconContainer}>
-            <Tooltip title={"Deselect ALL"}>
-                <IconButton className={classes.iconButton}>
-                    <DeleteIcon className={classes.icon} />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title={"Inverse selection"}>
-                <IconButton className={classes.iconButton} onClick={() => setOpenDialog(true)}>
-                    <GroupAddIcon className={classes.icon} />
-                </IconButton>
-            </Tooltip>
-            {/* Dialog */}
-            <Dialog
-                open={openDialog}
-                onClose={() => setOpenDialog(false)}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <form>
-                <DialogTitle id="alert-dialog-title">
-                  Add in bulk students to a cohort
-                  <div className="mt-4">
-                  <AsyncAutocomplete
-                    onChange={(cohort) => setCohort(cohort)}
-                    width={"100%"}
-                    size="medium"
-                    label="Cohort"
-                    required={true}
-                    getOptionLabel={option => `${option.name}, (${option.slug})`}
-                    asyncSearch={() => bc.admissions().getAllCohorts()}
-                    />
-                  </div>
-                </DialogTitle>
-                <DialogActions>
-                    <Button onClick={() => setOpenDialog(false)} color="primary">
-                        Cancel
-                    </Button>
-                    <Button color="primary" type="submit" autoFocus onClick={(e) => addBulkToCohort(e)}>
-                        Save
-                    </Button>
-                </DialogActions>
-                </form>
-            </Dialog>
-            {/* Dialog */}
-        </div>
+  const { classes } = props;
+  const [openDialog, setOpenDialog] = useState(false);
+  const [cohort, setCohort] = useState(null);
+  const [bulk, setBulk] = useState([]);
+
+  const selected = useMemo(() => {
+    return props.selectedRows.data.map((item) => item.index);
+  }, [props.selectedRows]);
+
+  useEffect(() => {
+    setBulk(
+      selected.map((item) => {
+        if (props.items[item].user !== null)
+          return {
+            user: props.items[item].user.id,
+            role: "STUDENT",
+            finantial_status: null,
+            educational_status: null,
+          };
+      })
     );
+  }, [selected]);
+  const addBulkToCohort = (e) => {
+    e.preventDefault();
+    bc.admissions()
+      .addUserCohort(cohort.id, bulk)
+      .then((d) => d)
+      .catch((r) => r);
+    setOpenDialog(false);
+  };
+  const deleteBulkStudents = (e) => {
+    e.preventDefault();
+    console.log(cohort.id);
+    bc.admissions()
+      .deleteUserCohortBulk(cohort.id, bulk)
+      .then((d) => d)
+      .catch((r) => r);
+  };
+  return (
+    <div className={classes.iconContainer}>
+      <Tooltip title={"Deselect ALL"}>
+        <IconButton className={classes.iconButton}>
+          <DeleteIcon
+            className={classes.icon}
+            onClick={(e) => {
+              setOpenDialog(true);
+            }}
+          />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title={"Inverse selection"}>
+        <IconButton
+          className={classes.iconButton}
+          onClick={() => setOpenDialog(true)}
+        >
+          <GroupAddIcon className={classes.icon} />
+        </IconButton>
+      </Tooltip>
+      {/* Dialog */}
+      <Dialog
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <form>
+          <DialogTitle id='alert-dialog-title'>
+            Add in bulk students to a cohort
+            <div className='mt-4'>
+              <AsyncAutocomplete
+                onChange={(cohort) => setCohort(cohort)}
+                width={"100%"}
+                size='medium'
+                label='Cohort'
+                required={true}
+                getOptionLabel={(option) => `${option.name}, (${option.slug})`}
+                asyncSearch={() => bc.admissions().getAllCohorts()}
+              />
+            </div>
+          </DialogTitle>
+          <DialogActions>
+            <Button onClick={() => setOpenDialog(false)} color='primary'>
+              Cancel
+            </Button>
+            <Button
+              color='primary'
+              type='submit'
+              autoFocus
+              onClick={(e) => addBulkToCohort(e)}
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+      {/* Dialog */}
+    </div>
+  );
 };
 
-
-export default withStyles(defaultToolbarSelectStyles, { name: "CustomToolbarSelect" })(CustomToolbarSelect);
+export default withStyles(defaultToolbarSelectStyles, {
+  name: "CustomToolbarSelect",
+})(CustomToolbarSelect);
--- src/app/services/breathecode.js
+++ src/app/services/breathecode.js
@@ -1,185 +1,354 @@
 import axios from "../../axios";
 
 class BreatheCodeClient {
-    constructor() {
-        this.host = `${process.env.REACT_APP_API_HOST}/v1`;
+  constructor() {
+    this.host = `${process.env.REACT_APP_API_HOST}/v1`;
+  }
+  admissions = () => {
+    return {
+      updateCohortUserInfo: (cohort, user, payload) => {
+        return axios._put(
+          "Cohort",
+          `${this.host}/admissions/cohort/${cohort}/user/${user}`,
+          payload
+        );
+      },
+      getAllUserCohorts: (query) => {
+        const qs = Object.keys(query)
+          .map((key) => `${key}=${query[key]}`)
+          .join("&");
+        return axios._get(
+          "Cohort",
+          `${this.host}/admissions/cohort/user?${qs}`
+        );
+      },
+      addUserCohort: (cohort, payload) => {
+        return axios._post(
+          "Cohort",
+          `${this.host}/admissions/cohort/${cohort}/user`,
+          payload
+        );
+      },
+      deleteUserCohort: (cohort, user) => {
+        return axios._delete(
+          "Cohort",
+          `${this.host}/admissions/cohort/${cohort}/user/${user}`
+        );
+      },
+      deleteUserCohortBulk: (cohort, payload) => {
+        return axios._delete(
+          "Cohort",
+          `${this.host}/admissions/cohort/${cohort}/user`,
+          payload
+        );
+      },
+      getCohort: (cohort) => {
+        return axios._get(
+          "Cohort",
+          `${this.host}/admissions/academy/cohort/${cohort}`
+        );
+      },
+      updateCohort: (cohort, payload) => {
+        return axios._put(
+          "Cohort",
+          `${this.host}/admissions/academy/cohort/${cohort}`,
+          payload
+        );
+      },
+      addCohort: (payload) => {
+        return axios._post(
+          "Cohort",
+          `${this.host}/admissions/academy/cohort`,
+          payload
+        );
+      },
+      getCertificates: () => {
+        return axios._get(
+          "Certificates",
+          `${this.host}/admissions/certificate`
+        );
+      },
+      getAllCohorts: (query) => {
+        const qs =
+          query !== undefined
+            ? Object.keys(query)
+                .map((key) => `${key}=${query[key]}`)
+                .join("&")
+            : "";
+        return axios._get(
+          "Cohorts",
+          `${this.host}/admissions/academy/cohort${query ? "?" + qs : ""}`
+        );
+      },
+      getAllCourseSyllabus: (query, academyID) => {
+        return axios._get(
+          "Syllabus",
+          `${this.host}/admissions/certificate/${query}/academy/${academyID}/syllabus`
+        );
+      },
+      getMyAcademy: () =>
+        axios._get("My Academy", `${this.host}/admissions/academy/me`),
+    };
+  };
+  auth() {
+    return {
+      addStudent: (payload) => {
+        return axios._post(
+          "Academy student",
+          `${this.host}/auth/academy/student`,
+          payload
+        );
+      },
+      getAllUsers: (query) => {
+        return axios._get("Users", `${this.host}/auth/user?like=${query}`);
+      },
+      getAcademyMember: (user) => {
+        return axios._get(
+          "Academy member",
+          `${this.host}/auth/academy/member/${user}`
+        );
+      },
+      addAcademyMember: (payload) => {
+        return axios._post(
+          "Academy member",
+          `${this.host}/auth/academy/member`,
+          payload
+        );
+      },
+      getRoles: () => {
+        return axios._get("Role", `${this.host}/auth/role`);
+      },
+      updateAcademyStudent: (user, payload) => {
+        return axios._put(
+          "Academy student",
+          `${this.host}/auth/academy/student/${user}`,
+          payload
+        );
+      },
+      updateAcademyMember: (user, payload) => {
+        return axios._put(
+          "Academy member",
+          `${this.host}/auth/academy/member/${user}`,
+          payload
+        );
+      },
+      addAcademyStudent: (payload) => {
+        return axios._post(
+          "Academy student",
+          `${this.host}/auth/academy/student`,
+          payload
+        );
+      },
+      getAcademyMembers: (query) => {
+        const qs = Object.keys(query)
+          .map((key) => `${key}=${query[key]}`)
+          .join("&");
+        return axios._get(
+          "Academy member",
+          `${this.host}/auth/academy/member?${qs}`
+        );
+      },
+      getAcademyStudents: (query) => {
+        const qs =
+          query !== undefined
+            ? Object.keys(query)
+                .map((key) => `${key}=${query[key]}`)
+                .join("&")
+            : "";
+        return axios._get(
+          "Academy student",
+          `${this.host}/auth/academy/student${query ? "?" + qs : ""}`
+        );
+      },
+      resendInvite: (user) => {
+        return axios._put(
+          "Invite",
+          `${this.host}/auth/member/invite/resend/${user}`
+        );
+      },
+    };
+  }
+  marketing = () => ({
+    getLeads: (query) => {
+      // start=${startDate.format('DD/MM/YYYY')}&academy=${academy}
+      const qs =
+        query !== undefined
+          ? Object.keys(query)
+              .map((key) => `${key}=${query[key]}`)
+              .join("&")
+          : "";
+      return axios._get(
+        "Lead report",
+        `${this.host}/marketing/report/lead${query ? "?" + qs : ""}`
+      );
+    },
+    getAcademyLeads: (query) => {
+      const qs =
+        query !== undefined
+          ? Object.keys(query)
+              .map((key) => `${key}=${query[key]}`)
+              .join("&")
+          : "";
+      return axios._get(
+        "Academy lead",
+        `${this.host}/marketing/academy/lead${query ? "?" + qs : ""}`
+      );
+    },
+    getAcademyTags: () =>
+      axios._get("Academy tags", `${this.host}/marketing/academy/tag`),
+    getAcademyAutomations: () =>
+      axios._get(
+        "Academy automations",
+        `${this.host}/marketing/academy/automation`
+      ),
+    addNewLead: (newLead) =>
+      axios._post("New lead", `${this.host}/marketing/lead`, newLead),
+  });
+
+  feedback = () => ({
+    getAnswers: (query) => {
+      // start=${startDate.format('DD/MM/YYYY')}&astatus=ANSWERED
+      const qs = Object.keys(query)
+        .map((key) => `${key}=${query[key]}`)
+        .join("&");
+      return axios._get(
+        "Academy answers",
+        `${this.host}/feedback/academy/answer?${qs}`
+      );
+    },
+    addNewSurvey: (newSurvey) =>
+      axios._post(
+        "New Survey",
+        `${this.host}/feedback/academy/survey`,
+        newSurvey
+      ),
+    updateSurvey: (survey, id) =>
+      axios._put(
+        "Survey",
+        `${this.host}/feedback/academy/survey/${id}`,
+        survey
+      ),
+  });
+  certificates = () => ({
+    getCertificatesByCohort: (query) => {
+      // start=${startDate.format('DD/MM/YYYY')}&astatus=ANSWERED
+      const qs = Object.keys(query)
+        .map((key) => `${key}=${query[key]}`)
+        .join("&");
+      return axios.get(`${this.host}/certificate/cohort/?${qs}`);
+    },
+  });
+  events = () => ({
+    getCheckins: (query) => {
+      // start=${startDate.format('DD/MM/YYYY')}status=${status}&event=${event_id}
+      const qs =
+        query !== undefined
+          ? Object.keys(query)
+              .map((key) => `${key}=${query[key]}`)
+              .join("&")
+          : "";
+      return axios._get(
+        "Event",
+        `${this.host}/events/academy/checkin${query ? "?" + qs : ""}`
+      );
+    },
+    addAcademyEvent: (payload) => {
+      return axios._post(
+        "Academy event",
+        `${this.host}/events/academy/event`,
+        payload
+      );
+    },
+    updateAcademyEvent: (event, payload) => {
+      return axios._put(
+        "Academy event",
+        `${this.host}/events/academy/event/${event}`,
+        payload
+      );
+    },
+    getAcademyEvents: (query) => {
+      const qs =
+        query !== undefined
+          ? Object.keys(query)
+              .map((key) => `${key}=${query[key]}`)
+              .join("&")
+          : "";
+      return axios._get(
+        "Academy event",
+        `${this.host}/events/academy/event${query ? "?" + qs : ""}`
+      );
+    },
+    getAcademyEvent: (event) => {
+      return axios._get(
+        "Academy event",
+        `${this.host}/events/academy/event/${event}`
+      );
+    },
+    getAcademyVenues: () => {
+      return axios._get("Venues", `${this.host}/events/academy/venues`);
+    },
+    getAcademyEventType: () => {
+      return axios._get("Event Type", `${this.host}/events/academy/eventype`);
+    },
+  });
+
+  certificates = () => ({
+    getAllCertificates: (query) => {
+      const qs =
+        query !== undefined
+          ? Object.keys(query)
+              .map((key) => `${key}=${query[key]}`)
+              .join("&")
+          : "";
+      return axios._get(
+        "Certificates",
+        `${this.host}/certificate${query ? "?" + qs : ""}`
+      );
+    },
+  });
+
+  media() {
+    return {
+      upload: (payload) => {
+        return axios._put("Media", `${this.host}/media/upload`, payload, {
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+      },
+      getAllCategories: () => {
+        return axios._get("Media", `${this.host}/media/category`);
+      },
+      getMedia: (query) => {
+        const qs =
+          query !== undefined
+            ? Object.keys(query)
+                .map((key) => `${key}=${query[key]}`)
+                .join("&")
+            : "";
+        return axios._get(
+          "Media",
+          `${this.host}/media${query ? "?" + qs : ""}`
+        );
+      },
+      updateMedia: (media, payload) => {
+        return axios._put("Media", `${this.host}/media/info/${media}`, payload);
+      },
+      deleteMedia: (media) => {
+        return axios._delete("Media", `${this.host}/media/info/${media}`);
+      },
+      createCategory: (payload) => {
+        return axios._post("Category", `${this.host}/media/category`, payload);
+      },
+    };
+  }
+
+  getItem(key) {
+    let value = this.ls.getItem(key);
+    try {
+      return JSON.parse(value);
+    } catch (e) {
+      return null;
     }
-    admissions = () => {
-        return {
-            updateCohortUserInfo: (cohort, user, payload) => {
-                return axios._put("Cohort",`${this.host}/admissions/cohort/${cohort}/user/${user}`, payload)
-            },
-            getAllUserCohorts: (query) => {
-                const qs = Object.keys(query).map(key => `${key}=${query[key]}`).join('&');
-                return axios._get("Cohort",`${this.host}/admissions/cohort/user?${qs}`);
-            },
-            addUserCohort: (cohort, payload) => {
-                return axios._post("Cohort",`${this.host}/admissions/cohort/${cohort}/user`, payload)
-            },
-            deleteUserCohort: (cohort, user) => {
-                return axios._delete("Cohort",`${this.host}/admissions/cohort/${cohort}/user/${user}`)
-            },
-            getCohort: (cohort) => {
-                return axios._get("Cohort",`${this.host}/admissions/academy/cohort/${cohort}`)
-            },
-            updateCohort: (cohort, payload) => {
-                return axios._put("Cohort",`${this.host}/admissions/academy/cohort/${cohort}`,payload)
-            },
-            addCohort: (payload) => {
-                return axios._post("Cohort",`${this.host}/admissions/academy/cohort`,payload)
-            },
-            getCertificates: () => {
-                return axios._get("Certificates",`${this.host}/admissions/certificate`)
-            },
-            getAllCohorts: (query) => {
-                const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : '';
-                return axios._get("Cohorts",`${this.host}/admissions/academy/cohort${query? '?'+ qs : ''}`)
-            },
-            getAllCourseSyllabus: (query, academyID) => {
-                return axios._get("Syllabus",`${this.host}/admissions/certificate/${query}/academy/${academyID}/syllabus`)
-            },
-            getMyAcademy: () => axios._get("My Academy",`${this.host}/admissions/academy/me`)
-         }
-    }
-    auth() {
-        return {
-            addStudent : (payload) => {
-                return axios._post("Academy student",`${this.host}/auth/academy/student`, payload)
-            },
-            getAllUsers: (query) => {
-                return axios._get("Users",`${this.host}/auth/user?like=${query}`)
-            },
-            getAcademyMember: (user) => {
-                return axios._get("Academy member",`${this.host}/auth/academy/member/${user}`)
-            },
-            addAcademyMember: (payload) => {
-                return axios._post("Academy member",`${this.host}/auth/academy/member`,payload)
-            },
-            getRoles: () => {
-                return axios._get("Role",`${this.host}/auth/role`)
-            },
-            updateAcademyStudent: (user, payload) => {
-                return axios._put("Academy student",`${this.host}/auth/academy/student/${user}`,payload)
-            },
-            updateAcademyMember: (user, payload) => {
-                return axios._put("Academy member",`${this.host}/auth/academy/member/${user}`,payload)
-            },
-            addAcademyStudent: (payload) => {
-                return axios._post("Academy student",`${this.host}/auth/academy/student`, payload)
-            },
-            getAcademyMembers: (query) => {
-                const qs = Object.keys(query).map(key => `${key}=${query[key]}`).join('&');
-                return axios._get("Academy member",`${this.host}/auth/academy/member?${qs}`)
-            },
-            getAcademyStudents: (query) => {
-                const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : ''; 
-                return axios._get("Academy student",`${this.host}/auth/academy/student${query ? '?'+ qs : ''}`)
-            },
-            resendInvite:(user) => {
-                return axios._put("Invite", `${this.host}/auth/member/invite/resend/${user}`)
-            }
-        }
-    }
-    marketing = () => ({
-        getLeads: (query) => {
-            // start=${startDate.format('DD/MM/YYYY')}&academy=${academy}
-            const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : '';
-            return axios._get("Lead report",`${this.host}/marketing/report/lead${query? '?'+ qs : ''}`)
-        },
-        getAcademyLeads: (query) => {
-            const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : '';
-            return axios._get("Academy lead", `${this.host}/marketing/academy/lead${query ? '?'+ qs : ''}`)
-        },
-        getAcademyTags: () => axios._get("Academy tags",`${this.host}/marketing/academy/tag`),
-        getAcademyAutomations: () => axios._get("Academy automations",`${this.host}/marketing/academy/automation`),
-        addNewLead: (newLead) => axios._post("New lead", `${this.host}/marketing/lead`, newLead)
-    })
-
-    feedback = () => ({
-        getAnswers: (query) => {
-            // start=${startDate.format('DD/MM/YYYY')}&astatus=ANSWERED
-            const qs = Object.keys(query).map(key => `${key}=${query[key]}`).join('&');
-            return axios._get("Academy answers",`${this.host}/feedback/academy/answer?${qs}`)
-        },
-        addNewSurvey: (newSurvey) => axios._post("New Survey", `${this.host}/feedback/academy/survey`, newSurvey),
-        updateSurvey: (survey, id) => axios._put("Survey", `${this.host}/feedback/academy/survey/${id}`, survey)
-    })
-    certificates = () => ({
-        getCertificatesByCohort: (query) => {
-            // start=${startDate.format('DD/MM/YYYY')}&astatus=ANSWERED
-            const qs = Object.keys(query).map(key => `${key}=${query[key]}`).join('&');
-            return axios.get(`${this.host}/certificate/cohort/?${qs}`)
-        },
-    })
-    events = () => ({
-        getCheckins: (query) => {
-            // start=${startDate.format('DD/MM/YYYY')}status=${status}&event=${event_id}
-            const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : '';
-            return axios._get("Event",`${this.host}/events/academy/checkin${query? '?'+ qs : ''}`)
-        },
-        addAcademyEvent: (payload) => {
-            return axios._post("Academy event",`${this.host}/events/academy/event`, payload)
-        },
-        updateAcademyEvent: (event, payload) => {
-            return axios._put("Academy event",`${this.host}/events/academy/event/${event}`, payload)
-        },
-        getAcademyEvents: (query) => {
-            const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : '';
-            return axios._get("Academy event",`${this.host}/events/academy/event${query ? '?'+ qs : ''}`);
-        },
-        getAcademyEvent: (event) => {
-            return axios._get("Academy event",`${this.host}/events/academy/event/${event}`)
-        },
-        getAcademyVenues: () => {
-            return axios._get("Venues", `${this.host}/events/academy/venues`)
-        },
-        getAcademyEventType: () => {
-            return axios._get("Event Type", `${this.host}/events/academy/eventype`)
-        }
-    });
-
-    certificates = () => ({
-        getAllCertificates: (query) => {
-            const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : '';
-            return axios._get("Certificates",`${this.host}/certificate${query ? '?'+ qs : ''}`)
-        }
-    });
-
-    media(){
-        return {
-            upload: (payload) => {
-                return axios._put("Media",`${this.host}/media/upload`, payload, {headers:{ 'Content-Type':'multipart/form-data'}})
-            },
-            getAllCategories: () => {
-                return axios._get("Media", `${this.host}/media/category`)
-            },
-            getMedia: (query) => {
-                const qs = query !== undefined ? Object.keys(query).map(key => `${key}=${query[key]}`).join('&') : '';
-                return axios._get('Media', `${this.host}/media${query ? '?'+ qs : ''}`)
-            },
-            updateMedia: (media,payload) => {
-                return axios._put("Media",`${this.host}/media/info/${media}`, payload)
-            },
-            deleteMedia: (media) => {
-                return axios._delete('Media',`${this.host}/media/info/${media}`)
-            },
-            createCategory : (payload) => {
-                return axios._post('Category', `${this.host}/media/category`, payload)
-            }
-        }
-    }
-
-    getItem(key) {
-        let value = this.ls.getItem(key)
-        try {
-            return JSON.parse(value)
-        } catch (e) {
-            return null
-        }
-    }
-
-    
-
+  }
 }
 
 export default new BreatheCodeClient();
