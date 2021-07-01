b1c50bf5f1f69df1b51e8e010b5cce9773a0baa2
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-01 18:08:42 -0400

    List view: reduced eslint errors

--- src/app/views/list/GridView.jsx
+++ src/app/views/list/GridView.jsx
@@ -1,23 +1,25 @@
-import React from "react";
-import { Card, Icon, Grid, Checkbox, Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
+import React from 'react';
+import {
+  Card, Icon, Grid, Checkbox, Button,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 const useStyles = makeStyles(({ palette, ...theme }) => ({
   gridCard: {
-    "& .grid__card-overlay": {
-      position: "absolute",
+    '& .grid__card-overlay': {
+      position: 'absolute',
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
       zIndex: 2,
       opacity: 0,
-      transition: "all 250ms ease-in-out",
-      background: "rgba(0, 0, 0, 0.67)",
+      transition: 'all 250ms ease-in-out',
+      background: 'rgba(0, 0, 0, 0.67)',
 
-      "& > div:nth-child(2)": {
-        position: "absolute",
+      '& > div:nth-child(2)': {
+        position: 'absolute',
         top: 0,
         bottom: 0,
         right: 0,
@@ -25,21 +27,21 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
         zIndex: -1,
       },
     },
-    "& .grid__card-bottom": {
-      "& .email": {
-        display: "none",
+    '& .grid__card-bottom': {
+      '& .email': {
+        display: 'none',
       },
     },
-    "&:hover": {
-      "& .grid__card-overlay": {
+    '&:hover': {
+      '& .grid__card-overlay': {
         opacity: 1,
       },
-      "& .grid__card-bottom": {
-        "& .email": {
-          display: "block",
+      '& .grid__card-bottom': {
+        '& .email': {
+          display: 'block',
         },
-        "& .date": {
-          display: "none",
+        '& .date': {
+          display: 'none',
         },
       },
     },
@@ -47,9 +49,9 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
 }));
 
 const calculateColumnPerRow = (value) => {
-  if (value === 25) {
-    return 2;
-  }
+  // if (value === 25) {
+  //   return 2;
+  // }
   if (value === 50) {
     return 3;
   }
@@ -59,6 +61,7 @@ const calculateColumnPerRow = (value) => {
   if (value === 100) {
     return 6;
   }
+  return 2;
 };
 
 const GridView = ({ list = [], sliderValue }) => {
@@ -69,51 +72,29 @@ const GridView = ({ list = [], sliderValue }) => {
       <Grid container spacing={2}>
         {list.map((item, index) => (
           <Grid item sm={calculateColumnPerRow(sliderValue)} key={item.id}>
-            <Card
-              className={clsx("flex-column h-full", classes.gridCard)}
-              elevation={6}
-            >
+            <Card className={clsx('flex-column h-full', classes.gridCard)} elevation={6}>
               <div className="grid__card-top text-center relative">
-                <img
-                  className="block w-full"
-                  src={item.projectImage}
-                  alt="project"
-                />
+                <img className="block w-full" src={item.projectImage} alt="project" />
                 <div className="grid__card-overlay flex-column">
                   <div className="flex items-center justify-between">
-                    <Checkbox className="text-white"></Checkbox>
+                    <Checkbox className="text-white" />
                     <div className="flex items-center">
-                      <Icon
-                        fontSize="small"
-                        className="mr-3 cursor-pointer text-white"
-                      >
+                      <Icon fontSize="small" className="mr-3 cursor-pointer text-white">
                         filter_none
                       </Icon>
-                      <Icon
-                        fontSize="small"
-                        className="mr-3 cursor-pointer text-white"
-                      >
+                      <Icon fontSize="small" className="mr-3 cursor-pointer text-white">
                         share
                       </Icon>
-                      <Icon
-                        fontSize="small"
-                        className="mr-3 cursor-pointer text-white"
-                      >
+                      <Icon fontSize="small" className="mr-3 cursor-pointer text-white">
                         edit
                       </Icon>
-                      <Icon
-                        fontSize="small"
-                        className="mr-3 cursor-pointer text-white"
-                      >
+                      <Icon fontSize="small" className="mr-3 cursor-pointer text-white">
                         delete
                       </Icon>
                     </div>
                   </div>
                   <div className="flex items-center justify-center">
-                    <Button
-                      variant="outlined"
-                      className="text-white border-color-white"
-                    >
+                    <Button variant="outlined" className="text-white border-color-white">
                       View Details
                     </Button>
                   </div>
