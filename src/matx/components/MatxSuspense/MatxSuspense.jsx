d40b8a80f4b2819fb9d8c0f0fdccc56f2b14b719
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-15 16:39:43 -0400

    husky finished

--- src/matx/components/MatxSuspense/MatxSuspense.jsx
+++ src/matx/components/MatxSuspense/MatxSuspense.jsx
@@ -10,40 +10,29 @@ const MatxSuspense = (props) => {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     if (props.loadbar) {
-      axios.interceptors.request.use(
-        (config) => {
-          // spinning start to show
-          setLoading(true);
-          return config;
-        },
-        (error) => {
-          setLoading(false);
-          return Promise.reject(error);
-        },
-      );
+      axios.interceptors.request.use((config) => {
+        // spinning start to show
+        setLoading(true);
+        return config;
+      }, (error) => {
+        setLoading(false);
+        return Promise.reject(error);
+      });
 
-      axios.interceptors.response.use(
-        (response) => {
-          // spinning hide
-          setLoading(false);
+      axios.interceptors.response.use((response) => {
+        // spinning hide
+        setLoading(false);
 
-          return response;
-        },
-        (error) => {
-          setLoading(false);
-          return Promise.reject(error);
-        },
-      );
+        return response;
+      }, (error) => {
+        setLoading(false);
+        return Promise.reject(error);
+      });
     }
   }, []);
 
   // eslint-disable-next-line react/jsx-one-expression-per-line
-  return (
-    <Suspense fallback={<MatxLoading />}>
-      {loading && props.loadbar && <LinearProgress />}
-      {props.children}
-    </Suspense>
-  );
+  return <Suspense fallback={<MatxLoading />}>{loading && props.loadbar && <LinearProgress />}{props.children}</Suspense>;
 };
 
 export default MatxSuspense;
