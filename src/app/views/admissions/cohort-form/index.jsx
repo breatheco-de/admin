54377aa6e3b1dc35e7115598b73ff0c9c58387ad
Alejandro Sanchez <aalejo@gmail.com>
2021-08-02 19:50:59 -0400

    Update index.jsx

--- src/app/views/admissions/cohort-form/index.jsx
+++ src/app/views/admissions/cohort-form/index.jsx
@@ -147,10 +147,10 @@ const Cohort = () => {
   };
 
   const updateCohort = (values) => {
-    const { endingDate, ...rest } = values;
+    const { ending_date, ...rest } = values;
     if (values.never_ends) {
       bc.admissions()
-        .updateCohort(cohort.id, { ...rest, private: cohort.private, endingDate: null })
+        .updateCohort(cohort.id, { ...rest, private: cohort.private, ending_date: null })
         .then((data) => data)
         .catch((error) => console.log(error));
     } else {
@@ -229,7 +229,7 @@ const Cohort = () => {
               <CohortDetails
                 slug={slug}
                 language={cohort.language || 'en'}
-                endDate={cohort.endingDate}
+                endDate={cohort.ending_date}
                 startDate={cohort.kickoff_date}
                 id={cohort.id}
                 syllabus={cohort.syllabus}
@@ -266,7 +266,7 @@ const Cohort = () => {
               name: cohort.name,
               language: cohort.language,
               kickoff_date: cohort.kickoff_date,
-              endingDate: cohort.endingDate,
+              ending_date: cohort.ending_date,
               current_day: stage === 'ENDED' ? maxSyllabusDays : currentDay,
             });
             setCohort({ ...cohort, stage, current_day: currentDay });
@@ -340,7 +340,7 @@ const Cohort = () => {
               name: cohort.name,
               language: cohort.language,
               kickoff_date: cohort.kickoff_date,
-              endingDate: cohort.endingDate,
+              ending_date: cohort.ending_date,
               current_day: currentDay,
             });
             setCohort({ ...cohort, current_day: currentDay });
