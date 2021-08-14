f6da9437f5b2ccb5aa97eb23b938e0daabd820ea
ArielCalisaya <calisayaariel62@gmail.com>
2021-08-14 17:44:13 -0400

    improving cypress tests

--- cypress/integration/login/index.spec.js
+++ cypress/integration/login/index.spec.js
@@ -1,5 +1,3 @@
-// <reference types="cypress" />
-
 describe('Login Screen', () => {
   beforeEach(() => {
     /*
@@ -77,60 +75,17 @@ describe('Login Screen', () => {
       */
       cy.get('[data-cy=login_form]');
       cy.fixture('/user_contact/right').then((right) => {
-        const { email, password } = right
+        const { email, password, token } = right
 
-        cy.log('**_____ Start intercept _____**')
-        //(old problem FIXED) line 117 on src/app/contexts/JWTAuthContext.jsx Cannot read property 'length' of undefined
-        cy.intercept('POST', '**/auth/login', {
-          statusCode: 200,
-          body: {
-            email: email,
-            password: password,
-            token: "nf4ifj83r98r4924f39df90ee081602cb3db7ec",
-            user_id: 238
-          },
-        }).as('postForm')
+        cy.log('**_____ Preparing Interception... _____**')
+        cy.intercept('POST', '**/auth/login').as('postForm')
 
-        cy.intercept('GET', '**/auth/token/*', {
-          status: 200,
-          token: "nf4ifj83r98r4924f39df90ee081602cb3db7ec",
-          token_type: "login",
-          expires_at: "2021-012-15T01:09:49.535548Z",
-          user_id: 238
-        }).as('getTokenStatus')
+        cy.mock_login(email, password, token);
+        cy.mock_token(token);
+        cy.mock_userData(email, password);
 
-        cy.intercept('GET', '**/auth/user/me', {
-          // statusCode: 200,
-          academy: {
-            id: 4,
-            name: "Downtown Miami",
-            slug: "downtown-miami"
-          },
-          email: email,
-          password: password,
-          first_name: 'userTesterME',
-          last_name: 'success?',
-          id: 136,
-          role: {
-            academy: {
-              id: 4,
-              name: "Downtown Miami",
-              slug: "downtown-miami"
-            },
-            role: "student"
-          },
-          roles: [
-            {
-              academy: {
-                id: 4,
-                name: "Downtown Miami",
-                slug: "downtown-miami"
-              },
-              role: "student"
-            }
-          ]
+        cy.intercept('GET', '**/auth/user/me').as('getUserData')
 
-        }).as('getUserData')
         /*
           here we use the "fixture" function where we have data of
           possible cases of credentials that would be better to validate
@@ -152,25 +107,21 @@ describe('Login Screen', () => {
         cy.get('[data-cy=submit_button]').click();
 
         // this method verifies that the user data has reached the api with the expected token
-        cy.log("**-------- Verifying Interception --------**")
+        // verify if the response has been intercepted and changed
+        cy.log("**-------- Intercepting api endpoint... --------**")
         cy.wait('@postForm');
-        // it verify if the response has been intercepted and changed
         cy.get('@postForm').then(xhr => {
           console.log("Response Intercepted:::",xhr)
-          // expect(xhr.response.statusCode).to.equal(200)
-          // expect(xhr.response.body.email).to.equal(email)
         })
 
         cy.wait('@getTokenStatus');
         cy.get('@getTokenStatus').then(xhr => {
-          console.log("Response getTokenStatus:::",xhr)
+          console.log("Response TokenStatus:::",xhr)
         })
 
         cy.wait('@getUserData');
         cy.get('@getUserData').then(xhr => {
-          console.log("Response getUserData:::",xhr)
-          // expect(xhr.response.statusCode).to.equal(200)
-          // expect(xhr.response.body.email).to.equal(email)
+          console.log("Response UserData:::",xhr)
         })
 
       });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,7 +11,197 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-//
+
+
+// NOTE: _____________________________ACCESS COMANDS_____________________________
+
+Cypress.Commands.add('handleLogin', () => {
+  cy.visit('/session/signin');
+  cy.log('**-------- Login process --------**');
+  cy.fixture('/user_contact/right').then((right) => {
+    const { email, password, token } = right
+
+    cy.mock_login(email, password, token);
+    cy.mock_token(token);
+    cy.mock_userData(email, password);
+
+    cy.get('[data-cy=email]').type(email);
+    cy.get('[data-cy=password]').type(password);
+    cy.get('[data-cy=submit_button]').click();
+
+    cy.wait('@postForm');
+    // it verify if the response has been intercepted and changed
+    cy.get('@postForm').then(xhr => {
+      console.log("Response Intercepted:::",xhr)
+    })
+    cy.wait('@getTokenStatus');
+    cy.get('@getTokenStatus').then(xhr => {
+      console.log("Response getTokenStatus:::",xhr)
+    })
+    cy.wait('@getUserData');
+    cy.get('@getUserData').then(xhr => {
+      console.log("Response getUserData:::",xhr)
+    })
+  })
+    
+});
+
+
+Cypress.Commands.add('mock_login', (email, password, token) => {
+  cy.intercept('POST', '**/auth/login', {
+    statusCode: 200,
+    body: {
+      email,
+      password,
+      token,
+      user_id: 238
+    },
+  }).as('postForm')
+});
+
+Cypress.Commands.add('mock_token', (token) => {
+  cy.intercept('GET', '**/auth/token/*', {
+    status: 200,
+    token,
+    token_type: "login",
+    expires_at: "2021-012-15T01:09:49.535548Z",
+    user_id: 238
+  }).as('getTokenStatus')
+});
+
+Cypress.Commands.add('mock_userData', (email, password) => {
+  cy.intercept('GET', '**/auth/user/me', {
+    academy: {
+      id: 4,
+      name: "Downtown Miami",
+      slug: "downtown-miami"
+    },
+    email,
+    password,
+    first_name: 'userTesterME',
+    last_name: 'success?',
+    id: 136,
+    role: {
+      academy: {
+        id: 4,
+        name: "Downtown Miami",
+        slug: "downtown-miami"
+      },
+      role: "admin"
+    },
+    roles: [
+      {
+        academy: {
+          id: 4,
+          name: "Downtown Miami",
+          slug: "downtown-miami"
+        },
+        role: "admin"
+      }
+    ]
+
+  }).as('getUserData')
+});
+
+
+// NOTE: _____________________________DASHBOARD COMANDS_____________________________
+
+Cypress.Commands.add('mock_leads', () => {
+  // "https://breathecode-test.herokuapp.com/v1/marketing/report/lead?start=2021-07-15&end=2021-08-14&academy=downtown-miami&by=utm_source"
+  cy.intercept('GET', '**/marketing/report/lead/*', {
+    status: 200,
+    statusText: "OK",
+    data: [
+      {
+        total_leads: 2,
+        utm_source: null
+      }
+    ]
+  }).as('getLead')
+});
+
+Cypress.Commands.add('mock_academyLeads', () => {
+  // "https://breathecode-test.herokuapp.com/v1/marketing/report/lead
+  cy.intercept('GET', '**/marketing/report/lead/*', {
+    status: 200,
+    statusText: "OK",
+    data: [
+      {
+        course: "software-engineering",
+        created_at__date: "2021-08-05",
+        created_date: "20210805",
+        location: "downtown-miami",
+        total_leads: 1
+      },
+      {
+        course: "software-engineering",
+        created_at__date: "2021-08-05",
+        created_date: "20210806",
+        location: "downtown-miami",
+        total_leads: 1
+      }
+    ]
+  }).as('getAcademyLeads')
+});
+
+Cypress.Commands.add('mock_academyEvents', () => {
+  cy.intercept('GET', '**/events/academy/checkin*', {
+    status: 200,
+    statusText: "OK",
+    data: []
+  }).as('getAcademyEvents')
+});
+
+Cypress.Commands.add('mock_feedbackAcademy', () => {
+  // responseURL: "https://breathecode-test.herokuapp.com/v1/feedback/academy/answer?status=ANSWERED"
+  cy.intercept('GET', '**/feedback/academy/answer?*', {
+    status: 200,
+    statusText: "OK",
+    data: [
+      {
+        academy: {
+          id: 4,
+          name: "Downtown Miami",
+          slug: "downtown-miami"
+        },
+        cohort: {
+          id: 6,
+          name: "MCBO-1",
+          slug: "mcbo-1"
+        },
+        comment: "Hello from cypress :)",
+        created_at: "2021-08-12T00:03:30.538849Z",
+        event: {
+          description: "description example",
+          excerpt: "etcmeidoemdie",
+          id: 3,
+          lang: "es",
+          title: "Programación orientada a objetos"
+        },
+        highest: "very likely",
+        id: 1,
+        lang: "en",
+        lowest: "not likely",
+        mentor: {
+          first_name: "Jhon",
+          id: 12,
+          last_name: "Doe"
+        },
+        score: 10,
+        status: "ANSWERED",
+        title: "Programación orientada a objetos",
+        user: {
+          first_name: "Crook",
+          id: 211,
+          last_name: "Doe"
+        }
+      },
+    ]
+  }).as('getFeedbackAcademy')
+});
+
+// TODO: Test functions work for dashboard cypress test
+
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
