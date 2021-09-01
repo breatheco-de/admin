2fe27b2c66cebbce3004d3ccf62f00a32690d7a1
israel vasquez <ingvi@MacBook-Air.local>
2021-09-01 13:55:59 -0400

    last test added url full validation

--- cypress/integration/students/index.spec.js
+++ cypress/integration/students/index.spec.js
@@ -64,6 +64,29 @@ describe('Students Screen', () => {
 
     });
 
+    it('Validating URL changes', () => {
+      cy.mock_search()
+
+      cy.log('**_____ Goin to Students Screen... _____**')
+      cy.visit('/admissions/students')
+
+      cy.log('**_____Making a search... _____**')
+      cy.get('[data-testid=Search-iconButton]').click()
+      cy.get('.MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('Jonathan{enter}')
+
+      cy.log('**_____Changing the number of rows per page shown to 40... _____**')
+      cy.get('[data-testid=pagination-rows]').click()
+      cy.get('[data-value="40"]').click()
+
+      cy.wait('@mock_search');
+      cy.get('@mock_search').then(xhr => {
+      console.log("Response search:::", xhr) 
+      })
+
+      cy.log('**_____Verifying that the url changes based on search and page config... _____**')
+      cy.url().should('include', 'limit=10&offset=0&like=Jonathan')
+
+    });
     
   });
 });
