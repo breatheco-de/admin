b64f7b0367cbd285ff940baaf2fba66d1d4a2295
israel vasquez <ingvi@MacBook-Air.local>
2021-09-06 14:32:28 -0400

    fixtures for students added

--- cypress/integration/students/index.spec.js
+++ cypress/integration/students/index.spec.js
@@ -1,4 +1,6 @@
+
 describe('Students Screen', () => {
+  
   beforeEach(() => {
     /*
       Cypress starts out with a blank slate for each test
@@ -6,15 +8,18 @@ describe('Students Screen', () => {
       Since we want to visit the same URL at the start of all our tests,
       we include it in our beforeEach function so that it runs before each test
     */
+    
     cy.handleLogin()
 
     cy.test_students() //what it does is send the student's data (mocked seeds)
     
   });
   context('Testing Students Screen', () => {
+     
     it('Validating backend response (mocked seeds)', () => {
         /* Hard coded structure to compare  */
         // cy.test_students()
+      cy.fixture('/students_screen_values/values').then((values) => {
 
         cy.log('**_____ Goin to Students Screen... _____**')
         cy.visit('/admissions/students')
@@ -23,69 +28,78 @@ describe('Students Screen', () => {
         cy.get('@test_students').then(xhr => {
         const students = xhr.response.body
         console.log("Response students:::",students) 
+        console.log("json students:::",values.search_url)
+
+      
+         
         
-        
-        }).its('response.body.results.length').should('be.eq', 2)
+        }).its('response.body.results.length').should('be.eq', values.students_count)
+      })
     });
+
     it('Validating pagination (offest and limit)', () => {
       // cy.test_students()
+      cy.fixture('/students_screen_values/values').then((values) => {
+        cy.log('**_____ Goin to Students Screen... _____**')
+        cy.visit('/admissions/students')
 
-      cy.log('**_____ Goin to Students Screen... _____**')
-      cy.visit('/admissions/students')
+        cy.log('**_____Changing the number of rows per page shown to 40... _____**')
+        cy.get('[data-testid=pagination-rows]').click()
+        cy.get('[data-value="40"]').click()
 
-      cy.log('**_____Changing the number of rows per page shown to 40... _____**')
-      cy.get('[data-testid=pagination-rows]').click()
-      cy.get('[data-value="40"]').click()
-
-      cy.log('**_____Verifying that the url changes with pagination... _____**')
-      cy.url().should('include', 'limit=40&offset=0')
-
+        cy.log('**_____Verifying that the url changes with pagination... _____**')
+        cy.url().should('include', values.pagination_url)
+      })
       // 
 
     });
 
     it('Validating search (like)', () => {
-      cy.mock_search()
 
-      cy.log('**_____ Goin to Students Screen... _____**')
-      cy.visit('/admissions/students')
+      cy.fixture('/students_screen_values/values').then((values) => { 
+        cy.mock_search()
 
-      cy.log('**_____Making a search... _____**')
-      cy.get('[data-testid=Search-iconButton]').click()
-      cy.get('.MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('Jonathan{enter}')
+        cy.log('**_____ Goin to Students Screen... _____**')
+        cy.visit('/admissions/students')
 
-      cy.wait('@mock_search');
-      cy.get('@mock_search').then(xhr => {
-      console.log("Response search:::", xhr) 
+        cy.log('**_____Making a search... _____**')
+        cy.get('[data-testid=Search-iconButton]').click()
+        cy.get('.MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(values.seach_word)
+
+        cy.wait('@mock_search');
+        cy.get('@mock_search').then(xhr => {
+        console.log("Response search:::", xhr) 
+        })
+
+        cy.log('**_____Verifying that the url changes with search... _____**')
+        cy.url().should('include', values.search_url)
       })
-
-      cy.log('**_____Verifying that the url changes with search... _____**')
-      cy.url().should('include', '&like=Jonathan')
-
     });
 
     it('Validating URL changes', () => {
-      cy.mock_search()
 
-      cy.log('**_____ Goin to Students Screen... _____**')
-      cy.visit('/admissions/students')
+      cy.fixture('/students_screen_values/values').then((values) => {
+        cy.mock_search()
 
-      cy.log('**_____Making a search... _____**')
-      cy.get('[data-testid=Search-iconButton]').click()
-      cy.get('.MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('Jonathan{enter}')
+        cy.log('**_____ Goin to Students Screen... _____**')
+        cy.visit('/admissions/students')
 
-      cy.log('**_____Changing the number of rows per page shown to 40... _____**')
-      cy.get('[data-testid=pagination-rows]').click()
-      cy.get('[data-value="40"]').click()
+        cy.log('**_____Making a search... _____**')
+        cy.get('[data-testid=Search-iconButton]').click()
+        cy.get('.MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(values.seach_word)
 
-      cy.wait('@mock_search');
-      cy.get('@mock_search').then(xhr => {
-      console.log("Response search:::", xhr) 
+        cy.log('**_____Changing the number of rows per page shown to 40... _____**')
+        cy.get('[data-testid=pagination-rows]').click()
+        cy.get('[data-value="40"]').click()
+
+        cy.wait('@mock_search');
+        cy.get('@mock_search').then(xhr => {
+        console.log("Response search:::", xhr) 
+        })
+
+        cy.log('**_____Verifying that the url changes based on search and page config... _____**')
+        cy.url().should('include', values.pagination_url + values.search_url)
       })
-
-      cy.log('**_____Verifying that the url changes based on search and page config... _____**')
-      cy.url().should('include', 'limit=10&offset=0&like=Jonathan')
-
     });
     
   });
