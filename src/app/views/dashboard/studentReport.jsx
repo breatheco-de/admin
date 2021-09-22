3d56cb7a56a600e3dc9eae5ec879b574c05e3c30
Alexson <alexsoncoelho21@hotmail.com>
2021-09-22 19:00:07 -0400

    improvement on activity grid

--- src/app/views/dashboard/studentReport.jsx
+++ src/app/views/dashboard/studentReport.jsx
@@ -17,7 +17,7 @@ const toastOption = {
 const studentReport = () => {
   const [query, setQuery] = useState({ limit: 10, offset: 0 });
   const [cohortUsersQuery, setCohortUsersQuery] = useState({
-    roles: 'TEACHER',
+    roles: 'TEACHER,ASSISTANT',
   });
   const { studentID, cohortID } = useParams();
   const [cohortData, setCohortData] = useState({});
@@ -84,7 +84,7 @@ const studentReport = () => {
         .getCohortActivity(cohortID, query)
         .then(({ data }) => {
           setActivitiesCount(data?.count);
-          setStudentActivity(data?.results || data);
+          setStudentActivity(data?.results || []);
         })
         .catch((err) => console.log(err));
     }
