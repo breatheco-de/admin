4039002080a1a2d04dfa3a6bc6f3af0174ef02be
Gustavo Marquez <gustavomm19@hotmail.com>
2022-03-08 16:16:40 -0400

    Add icon to image thumbnails

--- src/app/views/media/GridMediaCard.jsx
+++ src/app/views/media/GridMediaCard.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Icon, Tooltip } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
 import useAuth from '../../hooks/useAuth';
 
 import clsx from 'clsx';
