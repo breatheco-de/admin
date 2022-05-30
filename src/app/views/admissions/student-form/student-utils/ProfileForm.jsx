24d80418a1ee520d4fed12ff80712b53bb64d5d4
Alejandro Sanchez <aalejo@gmail.com>
2022-05-30 12:20:43 -0400

    Update ProfileForm.jsx

--- src/app/views/admissions/student-form/student-utils/ProfileForm.jsx
+++ src/app/views/admissions/student-form/student-utils/ProfileForm.jsx
@@ -28,7 +28,7 @@ export const ProfileForm = ({ initialValues }) => {
     };
     if (typeof (requestValues.invite) === 'undefined' || !requestValues.invite) requestValues.user = requestValues.id;
 
-    if (values.email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) === null) {
+    if (values.email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/) === null) {
       console.error("The email entered has formatting errors (insert a valid email address)")
       toast.error("The email entered has formatting errors (insert a valid email address)")
     }
