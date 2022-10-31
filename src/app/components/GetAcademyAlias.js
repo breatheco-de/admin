d7a2bbabe853a66298cebf10b58953c8c976b1a6
Roberto Lopez J <roberto@lightsaint.com>
2022-10-31 15:57:01 +0000

    [Made changes ]

--- src/app/components/GetAcademyAlias.js
+++ src/app/components/GetAcademyAlias.js
@@ -8,6 +8,8 @@ const GetAcademyAlias = () => {
     const [alias, setAlias] = useState([]);
 
     const { user } = useAuth();
+    const linktoWebPage = "4geeksacademy.notion.site/The-academy-alias-8c8e5844e27342e2bd85571e1ff87542"
+
 
     const getAcAcademyAlias = async () => {
         try {
@@ -30,15 +32,28 @@ const GetAcademyAlias = () => {
             setStatus({ color: "info", message: `Incoming leads are recognized by the following academy aliases: ${stringAlias} `});
         }
         else {
-            setStatus({ color: "warning", message: `This academy does not have an alias for its own slug, which means that incoming leads with the location ${user.academy.slug} will not be included in this list, you can read more about it here`});
+            setStatus({ color: "error", message: `This academy does not have an alias for its own slug, which means that incoming leads with the location ${user.academy.slug} will not be included in this list, you can `});
+            console.log(status)
         }
 
       }, [alias]);
 
+      let link = <a href="https://4geeksacademy.notion.site/The-academy-alias-8c8e5844e27342e2bd85571e1ff87542">read more about it here </a>;
+
+      function linkMessage() {
+        if (status.color == "error"){
+          return <p>This academy does not have an alias for its own slug, which means that incoming leads with the location {user.academy.slug} will not be included in this list, you can <a href="https://4geeksacademy.notion.site/The-academy-alias-8c8e5844e27342e2bd85571e1ff87542"><u>read more about it here.</u></a></p>;
+        }
+        else {
+          return status.message
+        }}
+  
+
   return (
     <div className='mb-2'>
     <Alert severity={status.color}>
-      <AlertTitle>{status.message}</AlertTitle>
+      <AlertTitle>{linkMessage()}
+      </AlertTitle>
       {/* Please past here you Eventbrite Key to begin the integration */}
     </Alert>
   </div>
