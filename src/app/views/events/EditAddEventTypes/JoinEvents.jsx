d1f04c686ac7e41c1d29ede53426b3526fb97fd8
Roberto Lopez J <roberto@lightsaint.com>
2023-02-16 00:28:37 +0000

    [Simplified the JoinEvents file]

--- src/app/views/events/EditAddEventTypes/JoinEvents.jsx
+++ src/app/views/events/EditAddEventTypes/JoinEvents.jsx
@@ -81,83 +81,83 @@ const JoinEvents = ({ eventype, onSubmit }) => {
           setSubmitting(false);
         }}
       >
-          {({ values, isSubmitting, handleChange, handleSubmit, setFieldValue }) => (
-            <Modal open={open} onClose={handleClose}>
-              <form className="p-4" onSubmit={handleSubmit}>
+        {({ values, isSubmitting, handleChange, handleSubmit, setFieldValue }) => (
+          <Modal open={open} onClose={handleClose}>
+            <form className="p-4" onSubmit={handleSubmit}>
 
-                <Box sx={style}>
-                  <Form className="p-4">
-                    <Grid item md={7} sm={8} xs={12}>
+              <Box sx={style}>
+                <Form className="p-4">
+                  <Grid item md={7} sm={8} xs={12}>
 
-                      <Grid item md={2} sm={4} xs={12}>
-                        Syllabus
-                      </Grid>
-
-                      <Grid item md={12} sm={12} xs={12}>
-                        <div className="flex flex-wrap">
-                          <AsyncAutocomplete
-                            debounced={false}
-                            onChange={(x) => setSyllabus(x)}
-                            width="100%"
-                            className="m-4"
-                            asyncSearch={() => bc.admissions().getAllSyllabus()}
-                            size="small"
-                            data-cy="syllabus"
-                            label="syllabus"
-                            required
-                            getOptionLabel={(option) => `${option.name}`}
-                            value={syllabus}
-                          />
-                          {syllabus ? (
-                            <AsyncAutocomplete
-                              className="m-4"
-                              debounced={false}
-                              onChange={(v) => setVersion(v)}
-                              width="30%"
-                              key={syllabus.slug}
-                              asyncSearch={() =>
-                                bc.admissions().getAllCourseSyllabus(syllabus.slug)
-                              }
-                              size="small"
-                              data-cy="version"
-                              label="Version"
-                              required
-                              getOptionLabel={(option) =>
-                                option.status === "PUBLISHED"
-                                  ? `${option.version}`
-                                  : "⚠️ " +
-                                  option.version +
-                                  " (" +
-                                  option.status +
-                                  ")"
-                              }
-                              value={version}
-                            />
-                          ) : (
-                            ""
-                          )}
-                        </div>
-                      </Grid>
-                      <Grid item md={2} sm={4} xs={12}>
-                        Cohort
-                      </Grid>
+                    <Grid item md={2} sm={4} xs={12}>
+                      Syllabus
                     </Grid>
-                    <div className="flex-column items-start px-4 mb-4">
-                      <Button
-                        color="primary"
-                        variant="contained"
-                        type="submit"
-                        data-cy="submit"
-                        disabled={isSubmitting}>
-                        Create
-                      </Button>
-                    </div>
-                  </Form>
-                </Box>
-              </form>
 
-            </Modal>
-          )}
+                    <Grid item md={12} sm={12} xs={12}>
+                      <div className="flex flex-wrap">
+                        <AsyncAutocomplete
+                          debounced={false}
+                          onChange={(x) => setSyllabus(x)}
+                          width="100%"
+                          className="m-4"
+                          asyncSearch={() => bc.admissions().getAllSyllabus()}
+                          size="small"
+                          data-cy="syllabus"
+                          label="syllabus"
+                          required
+                          getOptionLabel={(option) => `${option.name}`}
+                          value={syllabus}
+                        />
+                        {syllabus ? (
+                          <AsyncAutocomplete
+                            className="m-4"
+                            debounced={false}
+                            onChange={(v) => setVersion(v)}
+                            width="30%"
+                            key={syllabus.slug}
+                            asyncSearch={() =>
+                              bc.admissions().getAllCourseSyllabus(syllabus.slug)
+                            }
+                            size="small"
+                            data-cy="version"
+                            label="Version"
+                            required
+                            getOptionLabel={(option) =>
+                              option.status === "PUBLISHED"
+                                ? `${option.version}`
+                                : "⚠️ " +
+                                option.version +
+                                " (" +
+                                option.status +
+                                ")"
+                            }
+                            value={version}
+                          />
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </Grid>
+                    <Grid item md={2} sm={4} xs={12}>
+                      Cohort
+                    </Grid>
+                  </Grid>
+                  <div className="flex-column items-start px-4 mb-4">
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      type="submit"
+                      data-cy="submit"
+                      disabled={isSubmitting}>
+                      Create
+                    </Button>
+                  </div>
+                </Form>
+              </Box>
+            </form>
+
+          </Modal>
+        )}
       </Formik>
 
 
@@ -170,56 +170,50 @@ const JoinEvents = ({ eventype, onSubmit }) => {
 
           <div className="min-w-600">
             <>
-              {eventTypeVisbility != null ? eventype.visibility_settings.map((visibility, i) => (
-                <Grid key={i} item lg={12} md={12} sm={12} xs={12}>
-                  <div>
-                    <Grid className='m-2' item md={12} sm={12} xs={12}>
-                      <List>
-                        <ListItem>
-                          <div className='m-2'>
-                            {!visibility?.academy ? '' : `Everyone at the academy` + `${visibility?.academy.name}`}
-                          </div>
-                        </ListItem>
-                        <ListItem>
-                          <div className='m-2'>
-                            {!visibility?.academy
-                              || !visibility?.syllabus ? ''
-                              : `Only students from ${visibility.academy.name} with access to ${visibility.syllabus.name} syllabus.`}
-                          </div>
-                        </ListItem>
+              <Grid item lg={12} md={12} sm={12} xs={12}>
+                <div>
+                  <Grid className='m-2' item md={12} sm={12} xs={12}>
+                    {eventTypeVisbility != null ? eventype.visibility_settings.map((visibility, i) => (
+                      <>
+                        <List>
+                          <ListItem>
+                            <div key={i} className='m-2'>
+                              {!visibility?.academy ? '' : `Everyone at the academy ` + `${visibility?.academy.name}` + (i > 0 ? `, ${visibility[i + 1]?.academy.name}` : '')}
+                            </div>
+                          </ListItem>
+                          <ListItem>
+                            <div key={i} className='m-2'>
+                              {!visibility?.academy
+                                || !visibility?.syllabus ? ''
+                                : `Only students from ${visibility.academy.name} with access to ${visibility.syllabus.name} syllabus.`}
+                            </div>
+                          </ListItem>
 
-                        <ListItem>
-                          <div className='m-2'>
-                            {!visibility?.academy
-                              || !visibility?.syllabus
-                              || visibility?.cohort
-                              ? '' : `From cohort ${visibility?.cohort.name}`}
-                          </div>
-                        </ListItem>
-                      </List>
+                          <ListItem>
+                            <div key={i} className='m-2'>
+                              {!visibility?.academy
+                                || !visibility?.syllabus
+                                || visibility?.cohort
+                                ? '' : `From cohort ${visibility?.cohort.name}`}
+                            </div>
+                          </ListItem>
+                        </List>
+                      </>
+                    )) : (
+                      <>
+                        <div><h5>There are no shared settings</h5></div>
+                      </>
+                    )}
                       <IconButton onClick={() => {
-                        handleOpen()
-                      }}>
-                        <Icon fontSize="small">
-                          add_circle
-                        </Icon>
-                      </IconButton>
-                    </Grid>
-                  </div>
-                </Grid>
-              )) : (
-                <>
-                  <div><h5>There are no shared settings</h5></div>
-
-                  <IconButton onClick={() => {
-                    handleOpen()
-                  }}>
-                    <Icon fontSize="small">
-                      add_circle
-                    </Icon>
-                  </IconButton>
-                </>
-              )}
+                          handleOpen()
+                        }}>
+                          <Icon fontSize="small">
+                            add_circle
+                          </Icon>
+                        </IconButton>
+                  </Grid>
+                </div>
+              </Grid>
             </>
           </div>
 
