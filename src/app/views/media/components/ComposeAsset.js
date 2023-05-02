88c3f6e3b9d0e9ef828fcfc64667ddbcaf8d2362
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2023-05-02 12:31:52 -0400

    Update ComposeAsset.js

--- src/app/views/media/components/ComposeAsset.js
+++ src/app/views/media/components/ComposeAsset.js
@@ -398,7 +398,7 @@ const ComposeAsset = () => {
                   if (asset.status == 'PUBLISHED' && asset.published_at != null) setMakePublicDialog(true)
 
                   else {
-                    const _errors = await saveAsset(formattedDate); handleAction('push');
+                    const _errors = await saveAsset(formattedDate);
                     if (Object.keys(_errors).length > 0) setErrorDialog(true);
                   }
 
