ddce0beaad503709aec145683f04aca4412e6f71
avokeys <alexander.ayala.palacin@gmail.com>
2023-05-24 22:42:12 +0000

    udeleted code not being used in bultags.jsx

--- src/app/views/leads/settings-form/BulkTags.jsx
+++ src/app/views/leads/settings-form/BulkTags.jsx
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from "react";
 import {
-  TextField,
+
   Card,
-  Tooltip,
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogActions, Icon,IconButton
+   Icon,IconButton
 } from "@material-ui/core";
 import * as Yup from "yup";
-import { Formik } from "formik";
 import dayjs from "dayjs";
 import { SmartMUIDataTable } from "../../../components/SmartDataTable";
 import bc from "../../../services/breathecode";
-import BulkUpdateTag from "./BulkUpdateTag";
 import { useQuery } from "../../../hooks/useQuery";
-import { downloadFile } from '../../../utils';
+
 
 
 const relativeTime = require("dayjs/plugin/relativeTime");
@@ -28,22 +22,10 @@ const statusColors = {
   PENDING: "bg-warning text-dark",
 };
 
-const tagTpes = [
-  "STRONG",
-  "SOFT",
-  "DISCOVERY",
-  "COHORT",
-  "DOWNLOADABLE",
-  "EVENT",
-  "OTHER",
-];
 
 export const BulkTags = () => {
   const query = useQuery();
-  const [items, setItems] = useState([]);
   const [bulkItems, setBulkItems] = useState([]);
-  const [disputeIndex, setDisputeIndex] = useState(null);
-  const [disputedReason, setDisputedReason] = useState("");
   const [isHovering, setIsHovering] = useState(-1);
 
   Date.prototype.addDays = function (days) {
@@ -58,20 +40,9 @@ export const BulkTags = () => {
     return data;
   };
 
-  const deleteTime = (disputed_at) => {
-    const disputed = new Date(disputed_at);
-    const tenDays = disputed.addDays(10);
+  
 
-    if ((tenDays - new Date()) / (1000 * 60 * 60 * 24) > 1) {
-      const timeFromNow = dayjs(tenDays).fromNow(true);
-      return `Will de deleted in ${timeFromNow}`;
-    } else return "Will be deleted today";
-  };
-
-  const ProfileSchema = Yup.object().shape({
-    disputedReason: Yup.string().required("Please write the Disputed Reason"),
-  });
-
+  
   const columns = [
     {
       name: "file name", // field name in the row object
@@ -158,7 +129,7 @@ export const BulkTags = () => {
         filter: false,
         customBodyRenderLite: (i) => {
           const item = bulkItems[i];
-          console.log(item,"data item")
+          
           return (
             <div className="flex items-center">
               <div className="ml-3">
@@ -178,24 +149,14 @@ export const BulkTags = () => {
     },
   ];
 
-  const getTypes = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(tagTpes);
-      }, 500);
-    });
-  };
-
+  
   
   useEffect(() => {
     loadBulkData();
   }, []);
   return (
     <Card container className="p-4">
-      {/* <button onClick={()=>{
-        loadBulkData()
-      } 
-      }>click</button> */}
+  
       <SmartMUIDataTable
         title="Your recent uploads: "
         columns={columns}
@@ -207,104 +168,15 @@ export const BulkTags = () => {
           filter: false,
           customToolbar:false
         }}
-        view="student?"
-        historyReplace="/admissions/students"
         singlePage=""
       
-        // bulkActions={(props) => <AddBulkToCohort items={items} {...props} />}
+      //  page crashes when search prop is deleted
         search={async (querys) => {
           const { data } = await bc.auth().getAcademyStudents(querys);
-          setItems(data.results);
+          
           return data;
         }}
-        // deleting={async (querys) => {
-        //   const { status } = await bc.admissions().deleteStudentBulk(querys);
-        //   return status;
-        // }}
       />
-      {/* <Dialog
-        onClose={() => {
-          setDisputeIndex(null);
-          setDisputedReason("");
-        }}
-        // fullWidth
-        // maxWidth="md"
-
-        open={disputeIndex !== null}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      > */}
-        {/* <Formik
-          initialValues={{
-            disputedReason,
-          }}
-          enableReinitialize
-          validationSchema={ProfileSchema}
-          onSubmit={async (values) => {
-            const tag = items[disputeIndex];
-
-            const disputedAt = new Date();
-            const data = {
-              disputed_reason: disputedReason,
-              disputed_at: disputedAt,
-            };
-
-            const result = await bc
-              .marketing()
-              .updateAcademyTags(tag.slug, data);
-
-            if (result.status >= 200 && result.status < 300) {
-              const newItems = items;
-              newItems[disputeIndex] = {
-                ...newItems[disputeIndex],
-                ...data,
-              };
-              setItems(newItems);
-            }
-            setDisputeIndex(null);
-            setDisputedReason("");
-          }}
-        >
-          {({ errors, touched, handleSubmit }) => (
-            <form
-              onSubmit={handleSubmit}
-              className="d-flex justify-content-center mt-0"
-            >
-              <DialogTitle id="simple-dialog-title">
-                Write the dispute reason
-                <div className="mt-4">
-                  <TextField
-                    error={errors.disputedReason && touched.disputedReason}
-                    helperText={touched.disputedReason && errors.disputedReason}
-                    name="disputedReason"
-                    size="small"
-                    variant="outlined"
-                    value={disputedReason}
-                    onChange={(e) => setDisputedReason(e.target.value)}
-                    multiline
-                    rows={4}
-                    fullWidth
-                  />
-                </div>
-              </DialogTitle>
-              <DialogActions>
-                <Button
-                  onClick={() => {
-                    setDisputeIndex(null);
-                    setDisputedReason("");
-                  }}
-                  color="primary"
-                >
-                  Cancel
-                </Button>
-                <Button color="primary" type="submit" autoFocus>
-                  Send now
-                </Button>
-              </DialogActions>
-            </form>
-          )}
-        </Formik>
-      </Dialog> */}
     </Card>
   );
 };
