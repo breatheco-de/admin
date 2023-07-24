3f18fd1e94350f31ebfb80ea5451b4fcbf952c4f
Alejandro Sanchez <aalejo@gmail.com>
2023-07-24 20:07:55 +0000

    finished listing the provisioning bills

--- src/app/views/provisioning/github-form/ProvisioningBills.jsx
+++ src/app/views/provisioning/github-form/ProvisioningBills.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import {
   Grid,
-  IconButton,
-  Icon,
+  Button,
   Divider,
   Card,
   Table,
@@ -15,16 +14,20 @@ import {
   Box,
 } from "@material-ui/core";
 import dayjs from "dayjs";
+import { getToken, getSession } from '../../../redux/actions/SessionActions';
 const relativeTime = require("dayjs/plugin/relativeTime");
 
 dayjs.extend(relativeTime);
 
 export const ProvisioningBills = ({ className, bills }) => {
 
+  const token = getToken();
   const styles = {
     textAlign: 'center',
     width: '100%',
   };
+
+  const total_owed = bills.filter(current=> current.status == "DUE").reduce((prev, current) => prev + current.total_amount, 0);
 
   return (
     <Card container className={`p-4 ${className}`}>
@@ -33,7 +36,7 @@ export const ProvisioningBills = ({ className, bills }) => {
       </div>
       <Divider className="mb-2 flex" />
       <Grid item md={12}>
-      The following bills are pending and to be paid.
+      Your academy ows a total ammount of: ${total_owed}.
       </Grid>
       <Grid item md={12} className="mt-2">
         <Table>
@@ -42,29 +45,34 @@ export const ProvisioningBills = ({ className, bills }) => {
               <TableCell className="px-0">Name</TableCell>
               <TableCell className="px-0">Status</TableCell>
               <TableCell className="px-0">Amount</TableCell>
-              <TableCell className="px-0">Pay</TableCell>
+              <TableCell className="px-0"></TableCell>
+              <TableCell className="px-0"></TableCell>
             </TableRow>
           </TableHead>
             <TableBody>
               {bills.map((bill) => (
                 <TableRow key={bill.id}>
                   <TableCell className="pl-0 capitalize" align="left">
-                    {bill.name}
+                    {bill.title || "No title provided"}
                   </TableCell>
                   <TableCell className="pl-0 capitalize" align="left">
                     {bill.status}
                   </TableCell>
                   <TableCell className="pl-0 capitalize" align="left">
-                    {bill.total_amount}
+                    ${bill.total_amount}
+                  </TableCell>
+                  <TableCell className="pl-0 capitalize" align="left">
+                  <a className="underline pointer blue" href={`${process.env.REACT_APP_API_HOST}/v1/provisioning/bill/${bill.id}/html?token=${token}`} >details</a>
                   </TableCell>
                   <TableCell className="pl-0">
-                    <IconButton
-                      onClick={() => {
-                        window.open(bill.stripe_url)
-                      }}
+                    {bill.status == "DUE" && <Button
+                      size="small"
+                      variant="contained"
+                      color="primary"
+                      onClick={() => window.open(bill.stripe_url)}
                     >
-                      <Icon>attach_money</Icon>
-                    </IconButton>
+                      Pay
+                    </Button>}
                   </TableCell>
                 </TableRow>
               ))}
